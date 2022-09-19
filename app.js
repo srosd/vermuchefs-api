12fa5706d828dc64e2b5fb8c549cfaa415297fe8
Sergio <srosduran@gmail.com>
2022-09-19 09:18:40 +0200

    dotenv

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const mongoose = require('mongoose');
-const favicon       = require('serve-favicon');
-const cors          = require('cors');
+require('dotenv').config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const express        = require('express');
+const path           = require('path');
+const cookieParser   = require('cookie-parser');
+const logger         = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const mongoose       = require('mongoose');
+const favicon        = require('serve-favicon');
+const cors           = require('cors');
+
+const indexRouter    = require('./routes/index');
+const usersRouter    = require('./routes/users');
+
 
 mongoose
-  .connect(`mongodb+srv://admin:vermuteros@vermucluster.f8zdry0.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo:', err));
 
